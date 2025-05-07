36704d6f5b6aeae43ca5b554f0313a1cb281c9d7
Simmege <simon.erlen@outlook.com>
2025-05-07 10:34:04 +0200

    komplettering

--- js/mfu.js
+++ js/mfu.js
@@ -2,8 +2,13 @@
 
 document.querySelector(".lager").addEventListener("click", visaMeny)
 
-
 document.querySelector(".meny_knapp").addEventListener("click", visaMeny)
+
+document.querySelector("#x").addEventListener("click", doljReklam)
+
+document.querySelector("#x2").addEventListener("click", doljReklam2)
+
+
 
 function visaMeny(){
    
@@ -12,4 +17,20 @@ function visaMeny(){
 
     let layer = document.querySelector(".lager")
     layer.classList.toggle("visible")
+}
+
+function doljReklam(){
+    let reklam = document.querySelector("#reklam")
+    reklam.style.display = 'none';
+
+    let knapp = document.querySelector("#x")
+    knapp.style.display = 'none'
+}
+
+function doljReklam2(){
+    let reklam = document.querySelector("#reklam2")
+    reklam.style.display = 'none';
+
+    let knapp = document.querySelector("#x2")
+    knapp.style.display = 'none'
 }
